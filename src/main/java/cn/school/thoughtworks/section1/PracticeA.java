3582ae066d083620ac0a9ef5e16a3927d11524a3
915448715@qq.com <915448715@qq.com>
2018-03-31 12:02:16 +0800

    add 1-A

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,21 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> common = new ArrayList();
+        for(String str1 : collection1){
+            for(String str2 :collection2){
+                if(str1 == str2){
+                    common.add(str1);
+                }
+            }
+        }
+        return common;
     }
 }
